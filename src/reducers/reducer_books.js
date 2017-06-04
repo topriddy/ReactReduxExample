89cfcb31fd6b0d1e0cdd68d736eed414f3df8f5c
Temitope Faro <topriddy@gmail.com>
2017-06-04 19:34:08 +0100

    Completed the application

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    {title: 'JavaScript the Good Parts', pages: 301},
+    {title: 'Harry Porter', pages: 120},
+    {title: 'The Dark Tower', pages: 10},
+    {title: 'Eloquent Ruby', pages: 50}
+  ];
+}
